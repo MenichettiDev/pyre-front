50c1b9368ca7578013d46c1bd55bd23a63942b76
MenichettiDev <menichetti.dev@gmail.com>
2025-08-16 13:53:56 -0300

    Feat - Estructura y componentes: login login service auth service guard routes

--- pyre/src/app/app.routes.ts
+++ pyre/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from '../app/pages/login/login.component'; // Asegúrate de que la ruta sea correcta
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'login', pathMatch: 'full' }, // Redirige la ruta raíz a 'login'
+    { path: 'login', component: LoginComponent }, // Ruta para el componente Login
+];
